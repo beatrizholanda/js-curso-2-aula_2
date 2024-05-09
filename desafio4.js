4717d28ad8beda005e41159fb09b239ab08ec89f
ana <beatriz11holanda@gmail.com>
2024-05-08 22:35:43 -0300

    first commit

---
+++ desafio4.js
@@ -0,0 +1,14 @@
+let listaGenerica = [];
+
+let linguagensDeProgramação = ['JavaScript','C', 'C++', 'Kotlin', 'Python', 'Ruby', 'Java', 'GoLang'];
+console.log(linguagensDeProgramação);
+
+let nomes = ['Ana', 'João', 'Maria'];
+console.log(nomes[2]);
+
+let nome = ['Sofia', 'Guilherme', 'Beatriz'];
+console.log(nomes[1]);
+console.log(nome[nome.length - 1]);
+
+
+
